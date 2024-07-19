b3c6273cedea32275b6f75238c253a25f12e5a1e
zyrxakai <akshaib7557@gmail.com>
2024-07-19 13:37:18 +0530

    structure completed successfully, need to link url in some nav's

--- akai/products/urls.py
+++ akai/products/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('product_list', views.list_products, name='list_product'),
+    path('product_details', views.details_product, name='details_product')
 ]
--- akai/products/views.py
+++ akai/products/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'blank_layout.html')
+
+def list_products(request):
+    """"_summary_
+    returns product list page
+    Args:
+        request (_type_): _description_
+        
+    Returns:
+        _type_: _description_
+    """
+    return render(request, 'product.html')
+
+def details_product(request):
+    return render(request, 'product_details.html')
